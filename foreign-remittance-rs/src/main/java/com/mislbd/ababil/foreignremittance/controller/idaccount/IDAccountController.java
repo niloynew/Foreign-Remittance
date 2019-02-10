bc1bca924b0acc9f4a713da40338d7e39d0f10e1
maksudmisl <sajibpanthapth@gmail.com>
2019-02-10 17:46:22 +0600

    id account rest controller added

--- foreign-remittance-rs/src/main/java/com/mislbd/ababil/foreignremittance/controller/idaccount/IDAccountController.java
+++ foreign-remittance-rs/src/main/java/com/mislbd/ababil/foreignremittance/controller/idaccount/IDAccountController.java
@@ -1,13 +1,49 @@
 package com.mislbd.ababil.foreignremittance.controller.idaccount;
 
+import com.mislbd.ababil.foreignremittance.command.CreateIdAccountCommand;
+import com.mislbd.ababil.foreignremittance.domain.IdAccount;
+import com.mislbd.ababil.foreignremittance.service.IdAccountService;
+import com.mislbd.asset.command.api.CommandProcessor;
+import com.mislbd.asset.command.api.CommandResponse;
+import com.mislbd.asset.commons.data.domain.PagedResult;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.ResponseEntity.status;
 
 @RestController
 @RequestMapping(path = "/id-accounts", produces = MediaType.APPLICATION_JSON_VALUE)
 public class IDAccountController {
 
-    private final IdAccount
+    private final IdAccountService idAccountService;
+    private final CommandProcessor commandProcessor;
+
+
+    public IDAccountController(IdAccountService idAccountService, CommandProcessor commandProcessor) {
+        this.idAccountService = idAccountService;
+        this.commandProcessor = commandProcessor;
+    }
+
+    @GetMapping
+    public ResponseEntity<PagedResult<?>> findIdAccounts(
+            Pageable pageable
+
+            ) {
+
+        PagedResult<IdAccount> pagedAccounts = idAccountService.getAccounts(pageable);
+
+
+        return ResponseEntity.ok(pagedAccounts);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CommandResponse<Long>> createIdAccount(
+            @RequestBody IdAccount account) {
+        return status(CREATED)
+                .body(commandProcessor.executeResult(new CreateIdAccountCommand(account)));
+    }
 
 }
