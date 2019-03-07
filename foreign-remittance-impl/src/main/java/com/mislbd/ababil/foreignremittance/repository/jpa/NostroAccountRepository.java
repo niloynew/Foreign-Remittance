5436547ba1a8d6db7220228ff83178a52723001a
mahbub.hossain <mahbub.hossain@mislbd.com>
2019-03-07 11:46:32 +0600

    account removed

--- foreign-remittance-impl/src/main/java/com/mislbd/ababil/foreignremittance/repository/jpa/NostroAccountRepository.java
+++
@@ -1,6 +0,0 @@
-package com.mislbd.ababil.foreignremittance.repository.jpa;
-
-import com.mislbd.ababil.foreignremittance.repository.schema.NostroAccountEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NostroAccountRepository extends JpaRepository<NostroAccountEntity, Long> {}
