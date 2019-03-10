a875608e189e9b039460847426648b44cdce2c77
Miraj <miraj@mislbd.com>
2019-03-10 10:01:36 +0600

    Save work on account creation

---
+++ foreign-remittance-impl/src/main/java/com/mislbd/ababil/foreignremittance/command/handler/AccountCommandHandlerAggregate.java
@@ -0,0 +1,37 @@
+package com.mislbd.ababil.foreignremittance.command.handler;
+
+import com.mislbd.ababil.foreignremittance.command.SaveShadowAccountCommand;
+import com.mislbd.ababil.foreignremittance.domain.Account;
+import com.mislbd.ababil.foreignremittance.mapper.ShadowAccountMapper;
+import com.mislbd.ababil.foreignremittance.repository.jpa.ShadowAccountRepository;
+import com.mislbd.ababil.foreignremittance.repository.schema.ShadowAccountEntity;
+import com.mislbd.asset.command.api.CommandResponse;
+import com.mislbd.asset.command.api.annotation.Aggregate;
+import com.mislbd.asset.command.api.annotation.CommandHandler;
+import org.springframework.transaction.annotation.Transactional;
+
+@Aggregate
+public class AccountCommandHandlerAggregate {
+
+  private final ShadowAccountMapper shadowAccountMapper;
+  private final ShadowAccountRepository shadowAccountRepository;
+
+  public AccountCommandHandlerAggregate(
+      ShadowAccountMapper shadowAccountMapper, ShadowAccountRepository shadowAccountRepository) {
+    this.shadowAccountMapper = shadowAccountMapper;
+    this.shadowAccountRepository = shadowAccountRepository;
+  }
+
+  @Transactional
+  @CommandHandler
+  public CommandResponse<Void> saveShadowAccount(SaveShadowAccountCommand command) {
+    saveShadowAccount(command.getPayload());
+    return CommandResponse.asVoid();
+  }
+
+  private void saveShadowAccount(Account account) {
+    ShadowAccountEntity shadowAccountEntity = shadowAccountMapper.domainToEntity().map(account);
+    if (shadowAccountEntity != null) System.out.print("Almost done");
+    //            shadowAccountRepository.save(shadowAccountEntity);
+  }
+}
