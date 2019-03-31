60afb174b5ea93c25e84cb28ffb64c19a35182b5
mahbub.hossain <mahbub.hossain@mislbd.com>
2019-03-31 15:01:21 +0600

    idProduct pagination issue solved

--- foreign-remittance-impl/src/main/java/com/mislbd/ababil/foreignremittance/repository/jpa/IDProductRepository.java
+++ foreign-remittance-impl/src/main/java/com/mislbd/ababil/foreignremittance/repository/jpa/IDProductRepository.java
@@ -1,11 +1,8 @@
 package com.mislbd.ababil.foreignremittance.repository.jpa;
 
 import com.mislbd.ababil.foreignremittance.repository.schema.IDProductEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface IDProductRepository extends JpaRepository<IDProductEntity, Long> {
-  Page<IDProductEntity> findAll(Specification<IDProductEntity> idProduct, Pageable pageable);
-}
+public interface IDProductRepository
+    extends JpaRepository<IDProductEntity, Long>, JpaSpecificationExecutor {}
