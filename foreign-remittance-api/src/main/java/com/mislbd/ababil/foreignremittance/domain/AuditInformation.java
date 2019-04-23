8dd7fd8a5c81821403b748daacfebdaf97182f5b
shahriar miraj <miraj@mislbd.com>
2019-04-23 15:17:21 +0600

    Charge config added

--- foreign-remittance-api/src/main/java/com/mislbd/ababil/foreignremittance/domain/AuditInformation.java
+++ foreign-remittance-api/src/main/java/com/mislbd/ababil/foreignremittance/domain/AuditInformation.java
@@ -4,80 +4,80 @@ import java.time.LocalDate;
 
 public class AuditInformation {
 
-    private String entryUser;
+  private String entryUser;
 
-    private String entryTerminal;
+  private String entryTerminal;
 
-    private LocalDate entryDate;
+  private LocalDate entryDate;
 
-    private String verifyUser;
+  private String verifyUser;
 
-    private String verifyTerminal;
+  private String verifyTerminal;
 
-    private Integer userBranch;
+  private Integer userBranch;
 
-    private String processId;
+  private String processId;
 
-    public String getEntryUser() {
-        return entryUser;
-    }
+  public String getEntryUser() {
+    return entryUser;
+  }
 
-    public AuditInformation setEntryUser(String entryUser) {
-        this.entryUser = entryUser;
-        return this;
-    }
+  public AuditInformation setEntryUser(String entryUser) {
+    this.entryUser = entryUser;
+    return this;
+  }
 
-    public String getEntryTerminal() {
-        return entryTerminal;
-    }
+  public String getEntryTerminal() {
+    return entryTerminal;
+  }
 
-    public AuditInformation setEntryTerminal(String entryTerminal) {
-        this.entryTerminal = entryTerminal;
-        return this;
-    }
+  public AuditInformation setEntryTerminal(String entryTerminal) {
+    this.entryTerminal = entryTerminal;
+    return this;
+  }
 
-    public LocalDate getEntryDate() {
-        return entryDate;
-    }
+  public LocalDate getEntryDate() {
+    return entryDate;
+  }
 
-    public AuditInformation setEntryDate(LocalDate entryDate) {
-        this.entryDate = entryDate;
-        return this;
-    }
+  public AuditInformation setEntryDate(LocalDate entryDate) {
+    this.entryDate = entryDate;
+    return this;
+  }
 
-    public String getVerifyUser() {
-        return verifyUser;
-    }
+  public String getVerifyUser() {
+    return verifyUser;
+  }
 
-    public AuditInformation setVerifyUser(String verifyUser) {
-        this.verifyUser = verifyUser;
-        return this;
-    }
+  public AuditInformation setVerifyUser(String verifyUser) {
+    this.verifyUser = verifyUser;
+    return this;
+  }
 
-    public String getVerifyTerminal() {
-        return verifyTerminal;
-    }
+  public String getVerifyTerminal() {
+    return verifyTerminal;
+  }
 
-    public AuditInformation setVerifyTerminal(String verifyTerminal) {
-        this.verifyTerminal = verifyTerminal;
-        return this;
-    }
+  public AuditInformation setVerifyTerminal(String verifyTerminal) {
+    this.verifyTerminal = verifyTerminal;
+    return this;
+  }
 
-    public Integer getUserBranch() {
-        return userBranch;
-    }
+  public Integer getUserBranch() {
+    return userBranch;
+  }
 
-    public AuditInformation setUserBranch(Integer userBranch) {
-        this.userBranch = userBranch;
-        return this;
-    }
+  public AuditInformation setUserBranch(Integer userBranch) {
+    this.userBranch = userBranch;
+    return this;
+  }
 
-    public String getProcessId() {
-        return processId;
-    }
+  public String getProcessId() {
+    return processId;
+  }
 
-    public AuditInformation setProcessId(String processId) {
-        this.processId = processId;
-        return this;
-    }
+  public AuditInformation setProcessId(String processId) {
+    this.processId = processId;
+    return this;
+  }
 }
--- foreign-remittance-impl/src/main/java/com/mislbd/ababil/foreignremittance/service/DisbursementService.java
+++ foreign-remittance-impl/src/main/java/com/mislbd/ababil/foreignremittance/service/DisbursementService.java
@@ -9,69 +9,94 @@ import com.mislbd.ababil.foreignremittance.repository.schema.RemittanceTransacti
 import com.mislbd.ababil.foreignremittance.repository.schema.ShadowAccountEntity;
 import com.mislbd.ababil.transaction.domain.TransactionRequestType;
 import com.mislbd.ababil.transaction.service.TransactionService;
-import org.springframework.stereotype.Service;
-
+import java.math.BigDecimal;
 import java.util.List;
+import org.springframework.stereotype.Service;
 
 @Service
 public class DisbursementService {
 
-    private final TransactionService transactionService;
-    private final RemittanceTransactionMapper remittanceTransactionMapper;
-    private final ShadowAccountMapper shadowAccountMapper;
-    private final ShadowAccountRepository shadowAccountRepository;
+  private final TransactionService transactionService;
+  private final RemittanceTransactionMapper remittanceTransactionMapper;
+  private final ShadowAccountMapper shadowAccountMapper;
+  private final ShadowAccountRepository shadowAccountRepository;
 
-    public DisbursementService(TransactionService transactionService, RemittanceTransactionMapper remittanceTransactionMapper, ShadowAccountMapper shadowAccountMapper, ShadowAccountRepository shadowAccountRepository) {
-        this.transactionService = transactionService;
-        this.remittanceTransactionMapper = remittanceTransactionMapper;
-        this.shadowAccountMapper = shadowAccountMapper;
-        this.shadowAccountRepository = shadowAccountRepository;
+  public DisbursementService(
+      TransactionService transactionService,
+      RemittanceTransactionMapper remittanceTransactionMapper,
+      ShadowAccountMapper shadowAccountMapper,
+      ShadowAccountRepository shadowAccountRepository) {
+    this.transactionService = transactionService;
+    this.remittanceTransactionMapper = remittanceTransactionMapper;
+    this.shadowAccountMapper = shadowAccountMapper;
+    this.shadowAccountRepository = shadowAccountRepository;
+  }
+
+  public Long doTransaction(
+      RemittanceTransactionEntity remittanceTransactionEntity,
+      AuditInformation auditInformation,
+      List<RemittanceChargeInformation> charges) {
+
+    // Managing Debit transaction
+    ShadowAccountEntity shadowAccountEntity =
+        shadowAccountRepository
+            .findByNumber(remittanceTransactionEntity.getDebitAccountNumber())
+            .get();
+    transactionService.doGlTransaction(
+        remittanceTransactionMapper.getNetPayableGLDebit(
+            remittanceTransactionEntity,
+            shadowAccountEntity.getProduct().getProductGLCode(),
+            auditInformation),
+        TransactionRequestType.TRANSFER);
+
+    // Managing Credit transaction
+    switch (remittanceTransactionEntity.getCreditAccountType()) {
+      case GL:
+        transactionService.doGlTransaction(
+            remittanceTransactionMapper.getNetPayableGLCredit(
+                remittanceTransactionEntity, auditInformation),
+            TransactionRequestType.TRANSFER);
+        break;
+
+      case CASA:
+        transactionService.doCasaTransaction(
+            remittanceTransactionMapper.getNetPayableCASACredit(
+                remittanceTransactionEntity, auditInformation),
+            TransactionRequestType.TRANSFER);
+        break;
     }
 
+    // Managing charge transaction
+    BigDecimal totalCharge = BigDecimal.ZERO;
+    BigDecimal totalVat = BigDecimal.ZERO;
 
-    public Long doTransaction(RemittanceTransactionEntity remittanceTransactionEntity, AuditInformation auditInformation, List<RemittanceChargeInformation> charges) {
-
-        // Managing Debit transaction
-        ShadowAccountEntity shadowAccountEntity = shadowAccountRepository.findByNumber(remittanceTransactionEntity.getDebitAccountNumber()).get();
-        transactionService.doGlTransaction(remittanceTransactionMapper
-                .getNetPayableGLDebit(
-                        remittanceTransactionEntity,
-                        shadowAccountEntity.getProduct().getProductGLCode(),
-                        auditInformation),
-                TransactionRequestType.TRANSFER);
-
-        // Managing Credit transaction
-        switch (remittanceTransactionEntity.getCreditAccountType()) {
-            case GL:
-                transactionService.doGlTransaction(remittanceTransactionMapper
-                        .getNetPayableGLCredit(
-                                remittanceTransactionEntity,
-                                auditInformation),
-                        TransactionRequestType.TRANSFER);
-                break;
-
-            case CASA:
-                transactionService.doCasaTransaction(remittanceTransactionMapper
-                        .getNetPayableCASACredit(
-                                remittanceTransactionEntity,
-                                auditInformation),
-                        TransactionRequestType.TRANSFER);
-                break;
-        }
-        // Managing charge transaction
-        switch (remittanceTransactionEntity.getChargeAccountType()) {
-            case GL:
-
-                break;
-            case CASA:
-
-                break;
-        }
-
-        // manage exchange gain transaction
-//        transactionService.doGlTransaction(remittanceTransactionMapper.getExchangeGainGL)
-
-        return remittanceTransactionEntity.getGlobalTransactionNo();
+    if (charges != null && !charges.isEmpty()) {
+      charges.forEach(
+          charge -> {
+            totalCharge.add(charge.getChargeAmount());
+            totalVat.add(charge.getVatAmount());
+          });
     }
 
+    switch (remittanceTransactionEntity.getChargeAccountType()) {
+      case GL:
+        transactionService.doGlTransaction(
+            remittanceTransactionMapper.getNetPayableGLCredit(
+                remittanceTransactionEntity, auditInformation),
+            TransactionRequestType.TRANSFER);
+        break;
+      case CASA:
+        break;
+    }
+
+    // manage exchange gain transaction
+    transactionService.doGlTransaction(
+        remittanceTransactionMapper.getExchangeGainGL(
+            remittanceTransactionEntity,
+            shadowAccountEntity.getProduct().getExchangeGainGLCode(),
+            auditInformation),
+        TransactionRequestType.TRANSFER);
+
+    return remittanceTransactionEntity.getGlobalTransactionNo();
+  }
 }
