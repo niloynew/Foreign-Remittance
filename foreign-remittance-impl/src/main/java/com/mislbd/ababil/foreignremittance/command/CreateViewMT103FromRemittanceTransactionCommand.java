77deb9fee48d88961b432a0979040a352ad337bc
niloy <niloy@mislbd.com>
2020-10-22 16:48:12 +0600

    mt103

--- foreign-remittance-impl/src/main/java/com/mislbd/ababil/foreignremittance/command/CreateViewMT103FromRemittanceTransactionCommand.java
+++ foreign-remittance-impl/src/main/java/com/mislbd/ababil/foreignremittance/command/CreateViewMT103FromRemittanceTransactionCommand.java
@@ -2,13 +2,12 @@ package com.mislbd.ababil.foreignremittance.command;
 
 import com.mislbd.ababil.foreignremittance.domain.RemittanceTransaction;
 import com.mislbd.asset.command.api.Command;
-import com.mislbd.swift.broker.model.raw.mt1xx.MT103MessageRequest;
 
 public class CreateViewMT103FromRemittanceTransactionCommand
-        extends Command<RemittanceTransaction> {
-    public CreateViewMT103FromRemittanceTransactionCommand() {}
+    extends Command<RemittanceTransaction> {
+  public CreateViewMT103FromRemittanceTransactionCommand() {}
 
-    public CreateViewMT103FromRemittanceTransactionCommand(RemittanceTransaction payload) {
-        super(payload);
-    }
+  public CreateViewMT103FromRemittanceTransactionCommand(RemittanceTransaction payload) {
+    super(payload);
+  }
 }
