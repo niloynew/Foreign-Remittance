9822330322ecfd6bf7ac34baa8e1406327e2c54d
Syed Miraj <miraj@mislbd.com>
2020-11-01 09:54:15 +0600

    changes in consumer config

--- foreign-remittance-impl/src/main/java/com/mislbd/ababil/foreignremittance/configuration/KafkaConsumerConfig.java
+++ foreign-remittance-impl/src/main/java/com/mislbd/ababil/foreignremittance/configuration/KafkaConsumerConfig.java
@@ -4,41 +4,46 @@ import java.util.HashMap;
 import java.util.Map;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 @Configuration
 @EnableKafka
 public class KafkaConsumerConfig {
 
-  @Autowired private KafkaProperties kafkaProperties;
+  @Value("${spring.kafka.bootstrap-servers:192.168.1.140:9092}")
+  private String bootstrapServers;
+
+  @Bean
+  public Map<String, Object> consumerConfigs() {
+    Map<String, Object> props = new HashMap<String, Object>();
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, "swift-group");
+    return props;
+  }
+
+  @Bean
+  public ConsumerFactory<String, String> consumerFactory() {
+    return new DefaultKafkaConsumerFactory<String, String>(consumerConfigs());
+  }
+
+  @Bean
+  public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+    ConcurrentKafkaListenerContainerFactory<String, String> factory =
+        new ConcurrentKafkaListenerContainerFactory<String, String>();
+    factory.setConsumerFactory(consumerFactory());
+    return factory;
+  }
 
   @Bean
   public KafkaConsumer kafkaConsumer() {
     return new KafkaConsumer();
-  }
-
-  @Bean
-  public ConsumerFactory<String, Object> consumerFactory() {
-    final JsonDeserializer<Object> jsonDeserializer = new JsonDeserializer<>();
-    jsonDeserializer.addTrustedPackages("*");
-    Map<String, Object> props = new HashMap<>(kafkaProperties.buildConsumerProperties());
-    props.put(ConsumerConfig.GROUP_ID_CONFIG, "tpd-loggers");
-    return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), jsonDeserializer);
-  }
-
-  @Bean
-  public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
-    ConcurrentKafkaListenerContainerFactory<String, Object> factory =
-        new ConcurrentKafkaListenerContainerFactory<>();
-    factory.setConsumerFactory(consumerFactory());
-    return factory;
   }
 }
