1ed15688a170604f675c3836f880944b0b378be9
Syed Miraj <miraj@mislbd.com>
2021-02-23 10:58:07 +0600

    adding code validation

--- foreign-remittance-impl/src/main/java/com/mislbd/ababil/foreignremittance/command/handler/IDProductValidationCommandHandlerAggregate.java
+++ foreign-remittance-impl/src/main/java/com/mislbd/ababil/foreignremittance/command/handler/IDProductValidationCommandHandlerAggregate.java
@@ -2,14 +2,30 @@ package com.mislbd.ababil.foreignremittance.command.handler;
 
 import com.mislbd.ababil.foreignremittance.command.CreateIDProductCommand;
 import com.mislbd.ababil.foreignremittance.domain.IDProduct;
+import com.mislbd.ababil.foreignremittance.exception.ForeignRemittanceBaseException;
 import com.mislbd.ababil.foreignremittance.exception.IDProductCurrenciesNotFoundException;
+import com.mislbd.ababil.foreignremittance.repository.jpa.IDProductRepository;
+import com.mislbd.ababil.foreignremittance.repository.schema.IDProductEntity;
 import com.mislbd.asset.command.api.annotation.Aggregate;
+import com.mislbd.asset.command.api.annotation.ValidationHandler;
+import java.util.Optional;
 
 @Aggregate
 public class IDProductValidationCommandHandlerAggregate {
 
+  private final IDProductRepository productRepository;
+
+  public IDProductValidationCommandHandlerAggregate(IDProductRepository productRepository) {
+    this.productRepository = productRepository;
+  }
+
+  @ValidationHandler
   public void createIDProduct(CreateIDProductCommand command) {
     IDProduct idProduct = (IDProduct) command.getPayload();
+    Optional<IDProductEntity> productEntity = productRepository.findByCode(idProduct.getCode());
+    if (productEntity.isPresent()) {
+      throw new ForeignRemittanceBaseException("Code already used");
+    }
 
     if (idProduct.getCurrencies() == null || idProduct.getCurrencies().isEmpty()) {
       throw new IDProductCurrenciesNotFoundException();
