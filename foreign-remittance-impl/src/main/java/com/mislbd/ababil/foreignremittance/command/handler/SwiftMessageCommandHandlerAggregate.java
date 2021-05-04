c6d14bcdd87a52c340935b2040f07a432afbe07f
niloy <niloy@mislbd.com>
2021-05-04 13:45:56 +0600

    transaction correction

--- foreign-remittance-impl/src/main/java/com/mislbd/ababil/foreignremittance/command/handler/SwiftMessageCommandHandlerAggregate.java
+++ foreign-remittance-impl/src/main/java/com/mislbd/ababil/foreignremittance/command/handler/SwiftMessageCommandHandlerAggregate.java
@@ -27,131 +27,133 @@ import org.springframework.transaction.annotation.Transactional;
 @Aggregate
 public class SwiftMessageCommandHandlerAggregate {
 
-    private static final Logger LOGGER =
-            LoggerFactory.getLogger(SwiftMessageCommandHandlerAggregate.class);
-    private final ShadowTransactionRepository shadowTransactionRepository;
-    private final ModelMapper modelMapper;
-    private final Auditor auditor;
-    private final CommandProcessor commandProcessor;
-    private final SwiftMTMessageService swiftMTMessageService;
-    private final SwiftRegisterService swiftRegisterService;
-    private final ConfigurationService configurationService;
-    private final XmmIntegrationService xmmIntegrationService;
-    private String serviceURL = "http://192.168.1.104:8087/swift-service";
+  private static final Logger LOGGER =
+      LoggerFactory.getLogger(SwiftMessageCommandHandlerAggregate.class);
+  private final ShadowTransactionRepository shadowTransactionRepository;
+  private final ModelMapper modelMapper;
+  private final Auditor auditor;
+  private final CommandProcessor commandProcessor;
+  private final SwiftMTMessageService swiftMTMessageService;
+  private final SwiftRegisterService swiftRegisterService;
+  private final ConfigurationService configurationService;
+  private final XmmIntegrationService xmmIntegrationService;
+  private String serviceURL = "http://192.168.1.104:8087/swift-service";
 
-    public SwiftMessageCommandHandlerAggregate(
-            ShadowTransactionRepository shadowTransactionRepository,
-            ModelMapper modelMapper,
-            Auditor auditor,
-            CommandProcessor commandProcessor,
-            SwiftMTMessageService swiftMTMessageService,
-            SwiftRegisterService swiftRegisterService, ConfigurationService configurationService, XmmIntegrationService xmmIntegrationService) {
+  public SwiftMessageCommandHandlerAggregate(
+      ShadowTransactionRepository shadowTransactionRepository,
+      ModelMapper modelMapper,
+      Auditor auditor,
+      CommandProcessor commandProcessor,
+      SwiftMTMessageService swiftMTMessageService,
+      SwiftRegisterService swiftRegisterService,
+      ConfigurationService configurationService,
+      XmmIntegrationService xmmIntegrationService) {
 
-        this.shadowTransactionRepository = shadowTransactionRepository;
-        this.modelMapper = modelMapper;
-        this.auditor = auditor;
-        this.commandProcessor = commandProcessor;
-        this.swiftMTMessageService = swiftMTMessageService;
-        this.swiftRegisterService = swiftRegisterService;
-        this.configurationService = configurationService;
-        this.xmmIntegrationService = xmmIntegrationService;
-    }
+    this.shadowTransactionRepository = shadowTransactionRepository;
+    this.modelMapper = modelMapper;
+    this.auditor = auditor;
+    this.commandProcessor = commandProcessor;
+    this.swiftMTMessageService = swiftMTMessageService;
+    this.swiftRegisterService = swiftRegisterService;
+    this.configurationService = configurationService;
+    this.xmmIntegrationService = xmmIntegrationService;
+  }
 
-    @CommandListener(
-            commandClasses = {
-                    UpdateNostroTransactionCommand.class,
-                    ProcessNostroTransactionCommand.class
-            })
-    public void auditNostroReconcile(CommandEvent e) {
-        auditor.audit(e.getCommand().getPayload(), e.getCommand());
-    }
+  @CommandListener(
+      commandClasses = {
+        UpdateNostroTransactionCommand.class,
+        ProcessNostroTransactionCommand.class
+      })
+  public void auditNostroReconcile(CommandEvent e) {
+    auditor.audit(e.getCommand().getPayload(), e.getCommand());
+  }
 
-    @Transactional
-    @CommandHandler
-    public CommandResponse<Void> updateMessage(UpdateNostroTransactionCommand command) {
+  @Transactional
+  @CommandHandler
+  public CommandResponse<Void> updateMessage(UpdateNostroTransactionCommand command) {
 
-        NostroTransactionEntity nostroTransactionEntity =
-                modelMapper.map(command.getPayload(), NostroTransactionEntity.class);
+    NostroTransactionEntity nostroTransactionEntity =
+        modelMapper.map(command.getPayload(), NostroTransactionEntity.class);
 
-        shadowTransactionRepository.save(nostroTransactionEntity);
-        return CommandResponse.asVoid();
-    }
+    shadowTransactionRepository.save(nostroTransactionEntity);
+    return CommandResponse.asVoid();
+  }
 
-    @Transactional
-    @CommandHandler
-    public CommandResponse<Integer> processMessage(ProcessNostroTransactionCommand command) {
-        NostroAccountTransactionsDto dtoList = command.getPayload();
-        int success = 0;
-        if (dtoList.getNostroAccountTransactionList() != null
-                && !dtoList.getNostroAccountTransactionList().isEmpty()) {
-            for (NostroTransaction dto : dtoList.getNostroAccountTransactionList()) {
-                try {
-                    NostroTransactionEntity nostroTransactionEntity =
-                            modelMapper.map(dto, NostroTransactionEntity.class);
-                    nostroTransactionEntity.setSelected(true);
-                    shadowTransactionRepository.save(nostroTransactionEntity);
-                    success++;
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-            LOGGER.info(success + " nostro reconcile messages saved.");
+  @Transactional
+  @CommandHandler
+  public CommandResponse<Integer> processMessage(ProcessNostroTransactionCommand command) {
+    NostroAccountTransactionsDto dtoList = command.getPayload();
+    int success = 0;
+    if (dtoList.getNostroAccountTransactionList() != null
+        && !dtoList.getNostroAccountTransactionList().isEmpty()) {
+      for (NostroTransaction dto : dtoList.getNostroAccountTransactionList()) {
+        try {
+          NostroTransactionEntity nostroTransactionEntity =
+              modelMapper.map(dto, NostroTransactionEntity.class);
+          nostroTransactionEntity.setSelected(true);
+          shadowTransactionRepository.save(nostroTransactionEntity);
+          success++;
+        } catch (Exception e) {
+          e.printStackTrace();
         }
-        return CommandResponse.of(success);
+      }
+      LOGGER.info(success + " nostro reconcile messages saved.");
     }
+    return CommandResponse.of(success);
+  }
 
-    @Transactional
-    @CommandHandler
-    public CommandResponse<Void> publish103Message(
-            PublishSingleCustomerCreditTransferMessageCommand command) {
-        //    swiftMTMessageService.publish103message(serviceURL, command.getPayload());
-        // ToDo change when kafka integration complete
-//    ProcessResult processResult =
-//        swiftMTMessageService.save103message(serviceURL, command.getPayload());
-        MT103MessageRequest request = command.getPayload();
-        request.setEntryUser(String.valueOf(configurationService.getConfiguration("XMM_USER")));
-        request.setEntryUserBranch(
-                String.valueOf(configurationService.getConfiguration("XMM_USER_BRANCH")));
-        request.setTransactionReferenceNumber( request.getSendersReference());
-        request.setApplicationDate(configurationService.getCurrentApplicationDate());
-        /*if (processResult.getErrorCode() == 0) {
-            MessageResponse messageResponse =
-                    swiftMTMessageService.generate103message(serviceURL, command.getPayload());
-            swiftRegisterService.registerMessage(
-                    request.getSendersReference(),
-                    request.getSenderAddress(),
-                    request.getReceiverAddress(),
-                    messageResponse.getMessage());
-        }*/
-        xmmIntegrationService.publishCategoryNMessage(request);
-        return CommandResponse.asVoid();
-    }
-
-    @Transactional
-    @CommandHandler
-    public CommandResponse<ProcessResult> save103Message(
-            CreateSingleCustomerCreditTransferMessageCommand command) {
-        ProcessResult processResult =
-                swiftMTMessageService.save103message(serviceURL, command.getPayload());
-        MT103MessageRequest request = command.getPayload();
-        if (processResult.getErrorCode() == 0) {
-            MessageResponse messageResponse =
-                    swiftMTMessageService.generate103message(serviceURL, command.getPayload());
-            swiftRegisterService.registerMessage(
-                    request.getSendersReference(),
-                    request.getSenderAddress(),
-                    request.getReceiverAddress(),
-                    messageResponse.getMessage());
-        }
-        return CommandResponse.of(processResult);
-    }
-
-    @Transactional
-    @CommandHandler
-    public CommandResponse<MessageResponse> generate103Message(
-            GenerateSingleCustomerCreditTransferMessageCommand command) {
+  @Transactional
+  @CommandHandler
+  public CommandResponse<Void> publish103Message(
+      PublishSingleCustomerCreditTransferMessageCommand command) {
+    //    swiftMTMessageService.publish103message(serviceURL, command.getPayload());
+    // ToDo change when kafka integration complete
+    //    ProcessResult processResult =
+    //        swiftMTMessageService.save103message(serviceURL, command.getPayload());
+    MT103MessageRequest request = command.getPayload();
+    request.setEntryUser(String.valueOf(configurationService.getConfiguration("XMM_USER")));
+    request.setEntryUserBranch(
+        String.valueOf(configurationService.getConfiguration("XMM_USER_BRANCH")));
+    request.setTransactionReferenceNumber(request.getSendersReference());
+    request.setApplicationDate(configurationService.getCurrentApplicationDate());
+    /*if (processResult.getErrorCode() == 0) {
         MessageResponse messageResponse =
                 swiftMTMessageService.generate103message(serviceURL, command.getPayload());
-        return CommandResponse.of(messageResponse);
+        swiftRegisterService.registerMessage(
+                request.getSendersReference(),
+                request.getSenderAddress(),
+                request.getReceiverAddress(),
+                messageResponse.getMessage());
+    }*/
+    xmmIntegrationService.publishCategoryNMessage(request);
+    return CommandResponse.asVoid();
+  }
+
+  @Transactional
+  @CommandHandler
+  public CommandResponse<ProcessResult> save103Message(
+      CreateSingleCustomerCreditTransferMessageCommand command) {
+    ProcessResult processResult =
+        swiftMTMessageService.save103message(serviceURL, command.getPayload());
+    MT103MessageRequest request = command.getPayload();
+    if (processResult.getErrorCode() == 0) {
+      MessageResponse messageResponse =
+          swiftMTMessageService.generate103message(serviceURL, command.getPayload());
+      swiftRegisterService.registerMessage(
+          request.getSendersReference(),
+          request.getSenderAddress(),
+          request.getReceiverAddress(),
+          messageResponse.getMessage());
     }
+    return CommandResponse.of(processResult);
+  }
+
+  @Transactional
+  @CommandHandler
+  public CommandResponse<MessageResponse> generate103Message(
+      GenerateSingleCustomerCreditTransferMessageCommand command) {
+    MessageResponse messageResponse =
+        swiftMTMessageService.generate103message(serviceURL, command.getPayload());
+    return CommandResponse.of(messageResponse);
+  }
 }
