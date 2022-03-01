4352bb897f14d7240cc9902b55671154081fd302
Syed Miraj <miraj@mislbd.com>
2022-03-01 11:01:53 +0600

    initial validation in transaction

--- foreign-remittance-impl/src/main/java/com/mislbd/ababil/foreignremittance/command/CreateRemittanceTransactionCommand.java
+++ foreign-remittance-impl/src/main/java/com/mislbd/ababil/foreignremittance/command/CreateRemittanceTransactionCommand.java
@@ -2,14 +2,24 @@ package com.mislbd.ababil.foreignremittance.command;
 
 import com.mislbd.ababil.foreignremittance.domain.RemittanceTransaction;
 import com.mislbd.asset.command.api.Command;
+import com.mislbd.asset.command.api.HasIdentity;
 import com.mislbd.asset.command.api.annotation.CommandAttribute;
 
 @CommandAttribute(name = "Create Remittance Transaction", group = "FOREIGN REMITTANCE")
-public class CreateRemittanceTransactionCommand extends Command<RemittanceTransaction> {
+public class CreateRemittanceTransactionCommand extends Command<RemittanceTransaction>
+    implements HasIdentity {
+
+  private String transactionReferenceNumber;
 
   public CreateRemittanceTransactionCommand() {}
 
   public CreateRemittanceTransactionCommand(RemittanceTransaction payload) {
     super(payload);
+    this.transactionReferenceNumber = payload.getTransactionReferenceNumber();
+  }
+
+  @Override
+  public String getIdentity() {
+    return this.transactionReferenceNumber;
   }
 }
