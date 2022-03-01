004131c206dc2d2afb64f54f0c17ea9c6e7bd5ea
Syed Miraj <miraj@mislbd.com>
2022-03-01 15:55:04 +0600

    initial validation in transaction

--- foreign-remittance-impl/src/main/java/com/mislbd/ababil/foreignremittance/command/handler/RemittanceTransactionValidationCommandHandlerAggregate.java
+++ foreign-remittance-impl/src/main/java/com/mislbd/ababil/foreignremittance/command/handler/RemittanceTransactionValidationCommandHandlerAggregate.java
@@ -2,6 +2,7 @@ package com.mislbd.ababil.foreignremittance.command.handler;
 
 import com.mislbd.ababil.foreignremittance.command.CreateRemittanceTransactionCommand;
 import com.mislbd.ababil.foreignremittance.domain.RemittanceTransaction;
+import com.mislbd.ababil.foreignremittance.domain.RemittanceType;
 import com.mislbd.ababil.foreignremittance.exception.ForeignRemittanceBaseException;
 import com.mislbd.ababil.foreignremittance.external.service.CASAAccountService;
 import com.mislbd.asset.command.api.annotation.Aggregate;
@@ -18,11 +19,22 @@ public class RemittanceTransactionValidationCommandHandlerAggregate {
   }
 
   @ValidationHandler
-  public void validateInwardDisbursement(CreateRemittanceTransactionCommand command) {
+  public void validateRemittanceTransaction(CreateRemittanceTransactionCommand command) {
 
     RemittanceTransaction remittanceTransaction = command.getPayload();
     if (remittanceTransaction.getCbsTransactions().isEmpty()) {
       throw new ForeignRemittanceBaseException("No transaction leg found");
+    }
+    if (remittanceTransaction.getRemittanceType().equals(RemittanceType.INWARD_REMITTANCE)) {
+      if (remittanceTransaction.getSenderBIC() == null
+          || remittanceTransaction.getSenderBIC().isEmpty()) {
+        throw new ForeignRemittanceBaseException("Sender BIC not found");
+      }
+    } else {
+      if (remittanceTransaction.getReceiverBIC() == null
+          || remittanceTransaction.getReceiverBIC().isEmpty()) {
+        throw new ForeignRemittanceBaseException("Receiver BIC not found");
+      }
     }
   }
 }
