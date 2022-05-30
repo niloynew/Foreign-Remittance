7f1da00d2a3d896f3100c8f7ff26454d77d8697b
sakib <sakib@mislbd.com>
2022-05-30 15:17:30 +0600

    12903: Policy Library is needed in Ababil NG services

--- foreign-remittance-impl/src/main/java/com/mislbd/ababil/foreignremittance/configuration/StartupConfiguration.java
+++ foreign-remittance-impl/src/main/java/com/mislbd/ababil/foreignremittance/configuration/StartupConfiguration.java
@@ -3,6 +3,7 @@ package com.mislbd.ababil.foreignremittance.configuration;
 import com.mislbd.ababil.approvalflow.annotation.EnableAbabilApprovalflow;
 import com.mislbd.ababil.asset.annotation.EnableAbabilAsset;
 import com.mislbd.ababil.contacts.annotation.EnableAbabilContacts;
+import com.mislbd.ababil.security.policy.client.annotation.EnableAbabilPolicy;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,4 +12,5 @@ import org.springframework.context.annotation.Configuration;
 @EnableAbabilAsset
 @EnableAbabilContacts
 @EnableAbabilApprovalflow
+@EnableAbabilPolicy
 public class StartupConfiguration {}
