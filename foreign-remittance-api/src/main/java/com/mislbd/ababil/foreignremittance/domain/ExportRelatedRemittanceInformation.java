e7811b3083337f339639595ceb6e799f4eddb04c
Miraj <miraj@mislbd.com>
2022-07-27 10:35:19 +0600

    update tf export api

--- foreign-remittance-api/src/main/java/com/mislbd/ababil/foreignremittance/domain/ExportRelatedRemittanceInformation.java
+++ foreign-remittance-api/src/main/java/com/mislbd/ababil/foreignremittance/domain/ExportRelatedRemittanceInformation.java
@@ -1,6 +1,7 @@
 package com.mislbd.ababil.foreignremittance.domain;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
@@ -18,4 +19,7 @@ public class ExportRelatedRemittanceInformation {
   private Long customerId;
   private String applicantName;
   private String applicantCountry;
+  private LocalDate valueDate;
+  private String initiator;
+  private String verifier;
 }
